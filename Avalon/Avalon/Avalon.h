46ca55f378376ef263b0522d99b14e8bc5ac2439
ryouzhang <ryouzhang@gmail.com>
2016-05-21 22:45:11 +0800

    first version

---
+++ Avalon/Avalon/Avalon.h
@@ -0,0 +1,19 @@
+//
+//  Avalon.h
+//  Avalon
+//
+//  Created by RyouZhang on 5/21/16.
+//  Copyright © 2016 RyouZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Avalon.
+FOUNDATION_EXPORT double AvalonVersionNumber;
+
+//! Project version string for Avalon.
+FOUNDATION_EXPORT const unsigned char AvalonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Avalon/PublicHeader.h>
+
+
